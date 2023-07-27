9ee9aa0154c2b730cb73c80254eeb3a951230d77
Jonas Schopf <jonas.schopf@teilnehmer-bfwsb.de>
2023-07-27 08:57:01 +0200

    Jule hilft

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.site.register(Block)
+
+@admin.register(Block)
+
+class BlockAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'body', 'author', 'created', 'status']
+
+    list_filter = ['author', 'status']
+
+    search_fields = ['title', 'body']
+
+    prepopulated_fields = {'slug': ('title',)}
+
+    #raw_id_fields = ['author']
+
+    date_hierarchy = 'published'
+
+    ordering = ['status', 'published']
